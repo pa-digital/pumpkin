3a320528bb789b877dd7b0e696c1fd758d8e389a
Nick Dunn <nick.dunn@wearefriday.com>
2018-09-13 08:45:34 +0100

    Rewrite after feedback

--- README.md
+++ README.md
@@ -1,32 +1,24 @@
-# Cucumber reporter
+# Gherkin Test Report
 
-This application merges a Cucumber output report (from an automation suite) with a complete list of features and scenarios. It allows a tester to add the status for each scenario that is being tested manually.
+This application generates a test report directly from your gherkin feature files, so that your scenarios can be used as tests without the need for automation with Cucumber. If you have already automated some of your scenarios, the script accepts a Cucumber report (JSON) and it will pre-selet the pass/fail status of those implemented scenarios so that you can fill in the rest manually.
 
 ## Installation
 
 1. Clone the repo
 2. Run `bundle install` to install the dependencies
 
-## Set up Jenkins authentication
-
-The script pulls files directly out of Jenkins. Jenkins needs a username/password to log in, so you need to tell the script which details to authenticate with. To set this up:
-
-1. Log into Jenkins and click your username in the top right to view your profile
-2. Click "Configure" in the left navigation
-3. Under "API Token" click "Add new token" and give it a sensible description that describes this repo e.g. "Cucumber report generator"
-4. Copy the generated token to your clipboard
-5. Duplicate the file `.env.example` to `.env`
-6. Paste your token as the value for `JENKINS_API_KEY`, and your Jenkins username (firstname.lastname) for `JENKINS_USERNAME`
-
 ## Usage
 
-The script takes two input parameters:
+Run the script as follows:
 
-1. The URL of a Cucumber report
-2. The local path to your Gherkin feature files
+    $ ruby gherkin-test-report.rb --features /path/to/your/features/directory
 
-To find the Cucumber report, navigate to the test run in Jenkins (e.g. https://jenkins2.tools.fridayengineering.net/view/Aetna/job/aetna-vhealth-web-auto-tests/491/) and click "Cucumber reports" in the left navigation. This gives you the HTML report, but this scrpt needs the report in JSON format. To get this, strip the URL back to the `/cucumber-html-reports` section, and add the following to the URL: `.cache/json/cucumber.json`. This should show you a JSON file in the browser.
+The script accepts the following arguments:
 
-Example:
+* `--features` (or `-f`) is the path to a local directory of gherkin feature files
+* `--cucumberjson` (or `-c`) is the path to a local JSON file containing a Cucumber test report (optional)
+* `--open` (or `-o`) to open the HTML report in your browser after the script completes
 
-    ruby ./report.rb https://jenkins2.tools.fridayengineering.net/view/Aetna/job/aetna-vhealth-web-auto-tests/lastCompletedBuild/cucumber-html-reports/.cache/json/cucumber.json ~/src/aetna-vhealth-auto-tests/features
+### Where do I find the Cucumber JSON file?
+
+If you Cucumber tests are running on Jenkins, the "Cucumber HTML Reports" Jenkins plugin will provide a link from the build job to the HTML report (e.g. https://jenkins2.tools.fridayengineering.net/view/Aetna/job/aetna-vhealth-web-auto-tests/lastCompletedBuild/cucumber-html-reports/overview-steps.html). To get the same report in JSON format, strip the URL back to the `/cucumber-html-reports` section, and add the following to the URL: `.cache/json/cucumber.json` (e.g. https://jenkins2.tools.fridayengineering.net/view/Aetna/job/aetna-vhealth-web-auto-tests/lastCompletedBuild/cucumber-html-reports/.cache/json/cucumber.json). Save this JSON file to your computer.
---
+++ gherkin-test-report.rb
@@ -0,0 +1,126 @@
+#!/usr/bin/env ruby
+
+require 'cuke_modeler'
+require 'slop'
+require 'colored'
+
+def print_ok_message(message)
+  print ' ✔ '.green
+  puts message
+end
+
+def print_message(message)
+  puts message
+end
+
+def exit_with_error(message)
+  print ' ✘ '.red
+  puts message
+  exit 1
+end
+
+opts = Slop.parse do |o|
+  o.string '-f', '--features', 'path to your feature files', required: true
+  o.string '-c', '--cucumberjson', 'path to cucumber JSON report'
+  o.boolean '-o', '--open', 'open the HTML report in your browser when the script completes', default: false
+end
+
+FEATURE_FILE_PATH = opts[:features]
+CUCUMBER_REPORT = opts[:cucumberjson]
+OPEN_AFTER = opts[:open]
+FILENAME = 'output/report.html'
+
+def load_cucumber_json
+  return {} if CUCUMBER_REPORT.nil?
+  exit_with_error("#{CUCUMBER_REPORT} could not be found") unless File.exist?(CUCUMBER_REPORT)
+
+  JSON.parse(File.read(CUCUMBER_REPORT))
+end
+
+def load_feature_files
+  files = Dir.glob("#{FEATURE_FILE_PATH}/**/*.feature").map{|feature_file|
+    feature = CukeModeler::FeatureFile.new(feature_file)
+    print_message("Found feature '#{feature.feature.name}' with #{feature.feature.scenarios.count} scenarios")
+    feature
+  }
+  exit_with_error("No feature files found in #{FEATURE_FILE_PATH}") if files.count == 0
+  files
+end
+
+def scenario_status feature_name, scenario_name
+  status = ''
+  @report.each do |feature|
+    next unless feature["name"] == feature_name
+    feature['elements'].each do |scenario|
+      next unless scenario["name"] == scenario_name
+      scenario["steps"].each do |step|
+        status = step["result"]["status"]
+      end
+    end
+  end
+  status
+end
+
+def format_status status
+  return "<span class='scenario-status-print'></span><select class='scenario-status custom-select' style='width:130px;'>#{@status_types.map{|s| "<option #{'selected="selected"' if s.downcase == status}>#{s}</option>"}}</select>"
+end
+
+def format_steps scenario
+  html = '<p class="scenario-steps small text-muted" style="margin-bottom:0;margin-top:-.5em;">'
+  html += scenario.steps.map{|step| "#{step.keyword} #{step.text}"}.join('<br>')
+  html += '</p>'
+  html
+end
+
+@report = load_cucumber_json
+@feature_files = load_feature_files
+@status_types = ["Not run","Descoped", "In Progress", "Passed", "Failed", "Blocked"]
+
+open('output/report.html', 'w') { |f|
+  f << "<html>"
+  f << "<head>"
+  f << "<style type='text/css'>#{File.read('assets/bootstrap.min.css')}</style>"
+  f << "<style type='text/css'>#{File.read('assets/application.css')}</style>"
+  f << "<script>window.STATUS_TYPES = #{@status_types.to_json};</script>"
+  f << "<script>#{File.read('assets/jquery.js')}</script>"
+  f << "<script>#{File.read('assets/application.js')}</script>"
+  f << "</head>"
+  f << "<body>"
+  f << "<iframe id='iframe' style='display:none;'></iframe>"
+  f << "<div class='container'>"
+  f << "<span class='logo'></span>"
+  f << "<h1>Test Report<br><span class='report-title-print text-muted'></span> <span class='report-date-print text-muted'></span></h1>"
+  f << "<div class='form-group'><input type='text' placeholder='Project Name' class='form-control report-title'/></div>"
+  f << "<div class='form-group'><input type='text' placeholder='Date' class='form-control report-date' value='#{Time.now.strftime("%d/%m/%Y")}'/></div>"
+  f << "<div class='form-group'><textarea class='form-control report-description' placeholder='Notes'></textarea></div>"
+  f << "<a target='iframe' download='report.html' href='#' onclick='saveHTML();' class='btn btn-primary save-report'>Save HTML</a>"
+  f << "<h3>Summary</h3>"
+  f << "<p class='report-description-print'></p>"
+  f << "<table class='table'>"
+  f << "<thead><tr><th>Status</th><th>Total <span class='text-muted'>(<span class='total'></span>)</span></th></tr></thead>"
+  f << "<tbody id='results'></tbody>"
+  f << "</table>"
+  @feature_files.each do |feature_file|
+    feature_name = feature_file.feature.name
+    f << "<div class='feature'>"
+    f << "<h3>Feature: #{feature_name} <button class='btn btn-outline-secondary btn-sm float-right remove-feature'>Remove feature</button></h3>"
+    f << "<table class='table table-condensed'>"
+    f << "<thead><th colspan='2'>Scenario</th><th style='width:1px'>Status</th></thead>"
+    f << "<tbody>"
+    feature_file.feature.scenarios.each_with_index do |scenario, i|
+      scenario_name = scenario.name
+      scenario_status = scenario_status(feature_name, scenario_name)
+      f << "<tr class='scenario'><td style='width:1px' class='text-muted index'></td><td><button class='btn btn-outline-secondary btn-sm float-right remove-scenario' tabindex='-1'>Remove scenario</button><p>#{scenario_name.capitalize}</p>#{format_steps(scenario)}</td><td style='width:1px'>#{format_status(scenario_status)}</td></tr>"
+    end
+    f << "</tbody>"
+    f << "</table>"
+    f << "</div>"
+  end
+
+  f << "</div>"
+  f << "</body>"
+  f << "</html>"
+}
+
+print_ok_message("Report generated at #{FILENAME}")
+`open #{FILENAME}` if OPEN_AFTER
